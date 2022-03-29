a381348240388dd40843c69cbd3f72d811abc761
Alex <alexocegueda59@gmail.com>
2022-03-29 10:32:29 -0500

    created number_range function for validation

--- data_valid.py
+++ data_valid.py
@@ -1,5 +1,10 @@
-def yes_or_no():
-    prompt = "Enter yes or no"
+def yes_or_no(prompt):
+    """
+
+    :param prompt: Used to tell the user what they are saying yes or no to
+    :return: True if they mean yes and False if they mean No
+
+    """
 
     while True:
         user_input = input(f'{prompt} (y=yes, n=no): ').lower()
@@ -9,4 +14,26 @@ def yes_or_no():
         elif user_input in ['n', 'no']:
             return False
         else:
-            print('Invalid Input: Please enter yes or no (y=yes, n=no)')
+            print(f'Invalid Input: {prompt}')
+
+
+def valid_num_range(prompt, high, low):
+    """
+
+    :param prompt: Used to tell the user the range and prompt of what they are entering.
+    :param high: Enter the highest number that can be entered by users
+    :param low: Enter the lowest number that can be entered by users
+    :return: a valid number between the ranges specified
+    """
+    while True:
+        user_input = input(f'{prompt} (Valid {low}-{high}: ')
+
+        if type == 'int':
+            number = int(user_input)
+        else:
+            number = float(user_input)
+
+        if low <= number <= high:
+            return number
+        else:
+            print(f'Invalid input')
