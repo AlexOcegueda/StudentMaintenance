100e227dfe9dcf648957dd7c82074ec0e3e2f8fc
Kyleby21 <yates.kyle20@gmail.com>
2022-03-30 18:03:36 -0500

    Replaced list contents to actual names. Changed all double quotes to single quotes since it's the "python way".

--- main.py
+++ main.py
@@ -3,28 +3,28 @@ import menu
 
 
 def main():
-    students = [["Kyle Yates", 1975],
-                ["On the Waterfront", 1954],
-                ["Cat on a Hot Tin Roof", 1958]]
+    students = [['Bruno Zamora', 20],
+                ['Ela Cross', 30],
+                ['Amira Hogarth', 45]]
 
     menu.display_menu()
 
     while True:
-        command = input("Command: ")
-        if command == "1":
+        command = input('Command: ')
+        if command == '1':
             menu.list(students)
-        elif command == "2":
+        elif command == '2':
             menu.add(students)
-        elif command == "3":
+        elif command == '3':
             menu.delete(students)
-        elif command == "4":
+        elif command == '4':
             menu.update(students)
-        elif command == "5":
+        elif command == '5':
             break
         else:
-            print("Not a valid command. Please try again.\n")
-    print("Bye!")
+            print('Not a valid command. Please try again.\n')
+    print('Bye!')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
