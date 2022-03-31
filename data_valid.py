27338f01722738a8e126d957d1b4320069d9fc1d
Kyleby21 <yates.kyle20@gmail.com>
2022-03-31 15:16:22 -0500

    Created continue message function. Added more docstring and inline comments.

--- data_valid.py
+++ data_valid.py
@@ -1,12 +1,17 @@
+__program_name__ = "Student Maintenance"
+__author__ = "Kyle Yates, Alex Ocegueda"
+__version__ = "2.0"
+__github__ = "https://github.com/AlexOcegueda/StudentMaintenance"
+
 
 def yes_or_no(prompt):
     """
+    Yes or no prompt, where prompt is the message wished to be displayed.
+    If yes, return true and continue, if no return false.
 
-    :param prompt: Used to tell the user what they are saying yes or no to
+    param prompt: Used to tell the user what they are saying yes or no to
     :return: True if they mean yes and False if they mean No
-
     """
-
     while True:
         user_input = input(f'{prompt} (y=yes, n=no): ').lower()
 
@@ -20,21 +25,40 @@ def yes_or_no(prompt):
 
 def valid_num_range(prompt, high, low):
     """
+    A data validation where you specify the prompt, along with the high and low ranges
+    that the user can select from.
 
-    :param prompt: Used to tell the user the range and prompt of what they are entering.
-    :param high: Enter the highest number that can be entered by users
+    param prompt: Used to tell the user the range and prompt of what they are entering.
+    param high: Enter the highest number that can be entered by users
+    :param prompt: function defined prompt for valid num range
+    :param high: Enter the highest number that the user can enter.
     :param low: Enter the lowest number that can be entered by users
     :return: a valid number between the ranges specified
     """
     while True:
-        user_input = input(f'{prompt} (Valid {low}-{high}): ')
+        user_input = input(f'{prompt} (Valid {low}-{high}): ')  # user input of number between range
 
         if type == 'int':
             number = int(user_input)
         else:
             number = float(user_input)
 
-        if low <= number <= high:
+        if low <= number <= high:   # checks if between range
             return number
         else:
             print(f'Invalid input')
+
+
+def continue_message():
+    """
+    Simple function to print continue message.
+    Prevents storing anything other than space.
+    :return:
+        Empty character
+    """
+    userinput = input(f'\tPress enter to continue')
+    if userinput == "":
+        return userinput
+    else:
+        userinput = ''
+        return userinput
